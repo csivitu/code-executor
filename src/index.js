f2e5ec63174e9738b522cb0ebf6874ee8dfe3eb3
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-03 21:15:33 +0530

    fix: Modified index.ts

--- src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var shelljs_1 = require("shelljs");
-shelljs_1["default"].echo("hello world");
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+import util from 'util';
+import { exec } from 'child_process';
+
+const execute = util.promisify(exec);
 async function lsWithGrep() {
-  try {
-      const { stdout, stderr } = await exec('ls | grep js');
-      console.log('stdout:', stdout);
-      console.log('stderr:', stderr);
-  }catch (err)=>{
-     console.error(err);
-  };
-};
+    try {
+        const { stdout, stderr } = await execute('ls | grep js');
+        console.log('stdout:', stdout);
+        console.log('stderr:', stderr);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
 lsWithGrep();
