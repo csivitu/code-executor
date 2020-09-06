46889727c84b8df58bb7a539b4fc1965a9fb1a3d
ashikka <ashikagupta28@gmail.com>
2020-09-07 04:34:51 +0530

    feat: Added new module Runner

--- src/Runner.ts
+++ src/Runner.ts
@@ -3,7 +3,13 @@ import Docker from 'dockerode';
 export default class Runner {
     private docker: Docker;
 
+    readonly tag: string;
+
     constructor(docker: Docker) {
         this.docker = docker;
+    }
+
+    async runner(tag: string): Promise<void> {
+        await this.docker.run(tag, ['python', '-c', 'uname -a'], process.stdout);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import Docker from 'dockerode';
 
 import Builder from './Builder';
+import Runner from './Runner';
 
 const docker = new Docker();
 const builder = new Builder(docker);
-builder.build();
+builder.build('python-runner');
+const runner = new Runner(docker);
+runner.runner('python-runner');
