4612c0cb6c3ffe66bb3355f3e328ff7d24d17118
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-08 02:22:49 +0530

    feat: remove unnecessary module

--- src/models/Scheduler.ts
+++
@@ -1,9 +0,0 @@
-export default interface Job {
-    tag: string;
-    code: string;
-    options?: object;
-}
-
-export interface SchedulerOptions {
-    size: number;
-}
--- src/models/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as Job, SchedulerOptions } from './Scheduler';
