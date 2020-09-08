966573f55ee332d6f4853ceff42fd615783c13d9
ashikka <ashikagupta28@gmail.com>
2020-09-09 04:02:40 +0530

    feat: Created util logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,14 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+        }),
+    ],
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+    ),
+});
+
+export default logger;
