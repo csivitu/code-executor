558651efce9df18374946eb4ce83155593f097f0
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-23 14:51:26 +0530

    refactor: Constructor accepts docker, runner, builder objects

--- src/Worker.ts
+++ src/Worker.ts
@@ -3,46 +3,42 @@ import Bull from 'bull';
 import Runner from './Runner';
 import Builder from './Builder';
 
-const docker = new Docker();
-const runner = new Runner(docker);
-const builder = new Builder(docker);
-
-interface TestCase {
-    input: string;
-    output: string;
-}
-interface Code {
-    id: string;
-    code: string,
-    language: string,
-    testCases: TestCase[];
-}
+import { Code } from './models';
 
 export default class Worker {
     private redis: string;
 
     private name: string;
 
+    private runner: Runner;
+
+    private docker: Docker;
+
+    private builder: Builder;
+
     constructor(name: string, redis: string) {
         this.redis = redis;
         this.name = name;
+        this.docker = new Docker();
+        this.runner = new Runner(this.docker);
+        this.builder = new Builder(this.docker);
     }
 
     private queue = (() => new Bull(this.name, this.redis))();
 
-    private static async work(codeOptions: Code): Promise<void> {
+    private async work(codeOptions: Code): Promise<void> {
         const tag = `${codeOptions.language.toLowerCase()}-runner`;
         const { code } = codeOptions;
-        await runner.run({ tag, code, testCases: codeOptions.testCases });
+        await this.runner.run({ tag, code, testCases: codeOptions.testCases });
     }
 
-    static async build() {
-        await builder.build();
+    async build() {
+        await this.builder.build();
     }
 
     start() {
         this.queue.process(async (job, done) => {
-            await Worker.work(job.data);
+            await this.work(job.data);
             done();
         });
     }
