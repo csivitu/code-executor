744f200bc3270241827880b9c03327f9e8c33def
ashikka <ashikagupta28@gmail.com>
2020-09-24 04:47:26 +0530

    feat: Added new interfaces

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,8 +5,21 @@ export interface TestCase {
 
 export interface Code {
     id: string;
-    code: string,
-    language: string,
-    base64?: boolean,
+    code: string;
+    language: string;
+    base64?: boolean;
     testCases: TestCase[];
+}
+
+export interface Tests {
+    id: string;
+    input: string;
+    expectedOutput: string;
+    obtainedOutput: string;
+    remarks: string;
+    status: string;
+}
+
+export interface Result {
+    tests: Tests[];
 }
