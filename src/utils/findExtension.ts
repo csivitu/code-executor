f54d9419c64a453106cb8d316e54ad66c0709b5d
ashikka <ashikagupta28@gmail.com>
2020-09-24 04:44:28 +0530

    fix: Extension

--- src/utils/findExtension.ts
+++ src/utils/findExtension.ts
@@ -1,6 +1,6 @@
 export default function findExtension(language: string) {
     const extension = {
-        python: 'py', Bash: 'sh', C: 'c', Cplusplus: 'cpp', Golfscript: 'gs', Ruby: 'rb', Javascript: 'js', Java: 'java', Perl: 'pl', Swift: 'swift',
+        python: 'py', Bash: 'bash', C: 'c', Cplusplus: 'cpp', Golfscript: 'gs', Ruby: 'rb', Javascript: 'js', Java: 'java', Perl: 'pl', Swift: 'swift',
     };
     let fileExtension = '';
     Object.entries(extension).forEach((entry) => {
