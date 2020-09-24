3fb055f62619db1c4c698b7a534ece94efe209b4
ashikka <ashikagupta28@gmail.com>
2020-09-24 17:35:24 +0530

    feat: Script for all testcases

--- src/langs/Python/start.sh
+++ src/langs/Python/start.sh
@@ -1,1 +1,7 @@
-(cat /app/in1.txt | python3 /app/code.py) > /app/output1.txt
+source `${process.env.FOLDERPATH}`/code-executor/src/Runner.ts
+
+echo $RunnerOpts
+
+for i in RunnerOpts.length{
+(cat /app/`in${i}`.txt | python3 /app/code.py) > /app/`output${i}`.txt
+}
