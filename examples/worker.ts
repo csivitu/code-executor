fb610a0b1317ecf86b892daca71a189e8f8b0df9
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-29 23:38:55 +0530

    fix: worker.ts

--- examples/worker.ts
+++ examples/worker.ts
@@ -1,9 +1,9 @@
 import { Worker } from '../src/CodeExecutor';
 
 /**
-  *name, redis, folderPath, default folderPath is /tmp
-  *( folderPath will be mounted in container,
-  *the code and testcases will be saved here )
+  * name, redis, folderPath, default folderPath is /tmp/code-exec
+  * (folderPath will be mounted in container,
+  * the code and testcases will be saved here)
 */
 const worker = new Worker('myExecutor', 'redis://127.0.0.1:6379');
 
