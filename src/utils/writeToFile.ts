a638d9fadd8548c9c856ce9ebf92ab2f9b7157fc
alias-rahil <rahil.kabani.4@gmail.com>
2020-10-04 00:26:02 +0530

    fix: write permissions for folderpath

--- src/utils/writeToFile.ts
+++ src/utils/writeToFile.ts
@@ -3,8 +3,10 @@ import fs from 'fs';
 export default async function writeToFile(path: string, data: string): Promise<void> {
     return new Promise((resolve, reject) => {
         fs.writeFile(path, data, (err) => {
-            if (err) reject(err);
-            else resolve();
+            fs.chmod(path, '0777', () => {
+                if (err) reject(err);
+                else resolve();
+            });
         });
     });
 }
