24e66de646c3840c3e1a066d467a92d7f52abe50
Rohan Mukherjee <rohanbayern@gmail.com>
2020-11-04 00:23:31 +0530

    feat: CLI features added through class WorkerCLI

---
+++ src/WorkerCLI.ts
@@ -0,0 +1,63 @@
+import commander, { Command } from 'commander';
+import logger from './utils/logger';
+import { WorkerCLIOptions } from './models';
+import Worker from './Worker';
+
+class WorkerCLI {
+    readonly program: commander.Command;
+
+    readonly args: string[];
+
+    constructor(args: string[]) {
+        this.program = new Command();
+        this.args = args;
+        this.init();
+    }
+
+    init(): void {
+        this.program
+            .command('spawn-worker')
+            .alias('sw')
+            .description('spawn worker process')
+            .action(() => {
+                const options = this.parseOptions();
+                WorkerCLI.spawnWorker(options);
+            });
+
+        this.program
+            .option('-n, --number <number>', 'number of workers')
+            .option('-r, --redis <redis>', 'URL for the redis instance')
+            .option('-q, --queue <queue>', 'name of the redis queue')
+            .option('-l, --langs <langs...>', 'list of languages to build');
+    }
+
+    parseOptions(): WorkerCLIOptions {
+        const opts = this.program.opts();
+
+        const options: WorkerCLIOptions = {
+            number: opts.number || 1,
+            redis: opts.redis || 'redis://127.0.0.1:6379',
+            queue: opts.queue || 'myExecutor',
+            langs: opts.langs || [],
+        };
+
+        return options;
+    }
+
+    static async spawnWorker(options: WorkerCLIOptions): Promise<void> {
+        logger.info('Spawning Workers...');
+
+        const worker = new Worker(options.queue, options.redis);
+        await worker.build(options.langs.length ? options.langs : undefined);
+        worker.start();
+    }
+
+    async start(): Promise<void> {
+        await this.program.parseAsync(this.args);
+    }
+}
+
+export default async function cli(args: string[]): Promise<void> {
+    const workerCLI = new WorkerCLI(args);
+    workerCLI.start();
+}
